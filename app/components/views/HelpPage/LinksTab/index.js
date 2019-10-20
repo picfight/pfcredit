9b1b2f4c2016ee43f78029ff129d4ac4ba965427
J Fixby <github@jfixby.com>
2019-10-20 23:16:15 +0200

    update

--- app/components/views/HelpPage/LinksTab/index.js
+++ app/components/views/HelpPage/LinksTab/index.js
@@ -10,6 +10,13 @@ export const LinksTabHeader = () =>
 
 export const LinksTab = () => (
   <Aux>
+
+    <div className="tabbed-page-subtitle"><T id="help.subtitle.communications" m="Communications"/></div>
+    <div className="help-icons-list">
+      <HelpLink className={"help-forum-icon"} href="https://t.me/PicFightChat" title={<T id="help.telegram" m="Telegram" />} subtitle={<T id="help.telegram.subtitle" m="t.me/PicFightChat"/>}/>
+      <HelpLink className={"help-forum-icon"} href="https://vk.com/picfight" title={<T id="help.forum" m="VK" />} subtitle={<T id="help.forum.subtitle" m="vk.com/picfight"/>}/>
+    </div>
+
     <div className="tabbed-page-subtitle"><T id="help.subtitle.project" m="Project Related"/></div>
     <div className="help-icons-list">
       <HelpLink className={"help-github-icon"} href="https://github.com/picfight/" title={<T id="help.github.title" m="Github"/>} subtitle={<T id="help.github.subtitle" m="github.com/picfight/"/>} />
@@ -19,11 +26,6 @@ export const LinksTab = () => (
         subtitle={<T id="help.about.pfcredit.subtitle" m="Software Summary"/>}
       />
     </div>
-    <div className="tabbed-page-subtitle"><T id="help.subtitle.communications" m="Communications"/></div>
-    <div className="help-icons-list">
 
-      <HelpLink className={"help-forum-icon"} href="https://t.me/PicFightChat" title={<T id="help.telegram" m="Telegram" />} subtitle={<T id="help.telegram.subtitle" m="t.me/PicFightChat"/>}/>
-      <HelpLink className={"help-forum-icon"} href="https://vk.com/picfight" title={<T id="help.forum" m="VK" />} subtitle={<T id="help.forum.subtitle" m="vk.com/picfight"/>}/>
-    </div>
   </Aux>
 );
--- app/components/views/HelpPage/index.js
+++ app/components/views/HelpPage/index.js
@@ -1,6 +1,5 @@
 import { LinksTab, LinksTabHeader } from "./LinksTab";
 import { LogsTab, LogsTabHeader } from "./LogsTab";
-import { TutorialsTab, TutorialsTabHeader } from "./TutorialsTab";
 import { TabbedPage, TabbedPageTab as Tab, TitleHeader } from "layout";
 import { FormattedMessage as T } from "react-intl";
 import { Switch, Redirect } from "react-router-dom";
@@ -16,7 +15,6 @@ const HelpPage = () =>
   <TabbedPage header={<HelpPageHeader />}>
     <Switch><Redirect from="/help" exact to="/help/links" /></Switch>
     <Tab path="/help/links" component={LinksTab} header={LinksTabHeader} link={<T id="help.tab.sources" m="Sources"/>}/>
-    <Tab path="/help/tutorials" component={TutorialsTab} header={TutorialsTabHeader} link={<T id="help.tab.tutorials" m="Tutorials"/>}/>
     <Tab path="/help/logs" component={LogsTab} header={LogsTabHeader} link={<T id="help.tab.logs" m="Logs"/>}/>
   </TabbedPage>;
 
