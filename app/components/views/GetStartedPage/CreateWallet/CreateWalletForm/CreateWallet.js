0d5e63651ef621f645cfa678d987959137677b8a
J Fixby <github@jfixby.com>
2019-10-22 00:38:28 +0200

    -tooltip

--- app/components/views/GetStartedPage/CreateWallet/CreateWalletForm/CreateWallet.js
+++ app/components/views/GetStartedPage/CreateWallet/CreateWalletForm/CreateWallet.js
@@ -23,7 +23,7 @@ const CreateWallet = ({
   <Aux>
     <div className="getstarted content">
       <div className="go-back-screen-button-area">
-        <Tooltip text={ <T id="createWallet.goBack" m="Go back" /> }><div className="go-back-screen-button" onClick={onReturnToWalletSelection}/></Tooltip>
+        <div className="go-back-screen-button" onClick={onReturnToWalletSelection}/>
       </div>
       <div className="content-title">
         <CreateWalletTitleMsg />
