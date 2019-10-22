c4c83e04153d95f64e7edc432dcc117cfab8d52a
J Fixby <github@jfixby.com>
2019-10-22 13:00:19 +0200

    vk link

--- app/components/views/HelpPage/LinksTab/index.js
+++ app/components/views/HelpPage/LinksTab/index.js
@@ -14,12 +14,12 @@ export const LinksTab = () => (
     <div className="tabbed-page-subtitle"><T id="help.subtitle.communications" m="Communications"/></div>
     <div className="help-icons-list">
       <HelpLink className={"help-forum-icon"} href="https://t.me/PicFightChat" title={<T id="help.telegram" m="Telegram" />} subtitle={<T id="help.telegram.subtitle" m="t.me/PicFightChat"/>}/>
-      <HelpLink className={"help-forum-icon"} href="https://vk.com/picfight" title={<T id="help.forum" m="VK" />} subtitle={<T id="help.forum.subtitle" m="vk.com/picfight"/>}/>
+      <HelpLink className={"help-forum-icon"} href="https://vk.com/picfight" title={<T id="help.vk" m="VK" />} subtitle={<T id="help.vk.subtitle" m="vk.com/picfight"/>}/>
     </div>
 
     <div className="tabbed-page-subtitle"><T id="help.subtitle.project" m="Project Related"/></div>
     <div className="help-icons-list">
-      <HelpLink className={"help-github-icon"} href="https://github.com/picfight/" title={<T id="help.github.title" m="Github"/>} subtitle={<T id="help.github.subtitle" m="github.com/picfight/"/>} />
+      <HelpLink className={"help-github-icon"} href="https://github.com/picfight/pfcredit/issues" title={<T id="help.github.title" m="Github"/>} subtitle={<T id="help.github.subtitle" m="github.com/picfight/"/>} />
       <HelpLink className={"help-blockchain-explorer-icon"} href="http://explorer.picfight.org" title={<T id="help.blockchain" m=" Blockchain Explorer" />} subtitle={<T id="help.blockchain.subtitle" m="explorer.picfight.org"/>}/>
       <HelpLinkAboutModal className={"help-star-icon"}
         title={<T id="help.about.pfcredit" m="About Pfcredit"/>}
